ad9f554d9e702f807fceab60598768f1b7f8f5a4
Niponpal <niponpal52@gmail.com>
2025-12-03 21:06:47 +0600

    Fix 🚀🚀🚑 Buyer Register Pages

--- CatMS/obj/Debug/net8.0/CatMS.AssemblyInfo.cs
+++ CatMS/obj/Debug/net8.0/CatMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+115f6985556b39ee44e940173d3bd029969b9478")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+759abab178b2c3aeb15bd60a42cf03fe63e985ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
