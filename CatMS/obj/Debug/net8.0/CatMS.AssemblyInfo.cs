801486278e1dac57c244eac8e50e40c414739e6d
Niponpal <niponpal52@gmail.com>
2025-12-22 19:04:05 +0600

    Fix 🚀🚀🚑

--- CatMS/obj/Debug/net8.0/CatMS.AssemblyInfo.cs
+++ CatMS/obj/Debug/net8.0/CatMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2efc26bb6101beb6a84b647a72c3af072956f225")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b6e587357f85f292863f545bbcdde5ae404cdc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
